8f340afd15a4316454504005d1abf804ec4e7ea9
CeciliaMartinezMartin <43567723+CeciliaMartinezMartin@users.noreply.github.com>
2019-12-18 12:58:32 +0100

    Clean Code

--- ComputephiSunAbsolute.m
+++ ComputephiSunAbsolute.m
@@ -1,15 +1,20 @@
-function phiSun = ComputephiSunAbsolute (t, days, hdays, rango)
-    % This function returns the position of the Sun with respect to the
-    % Moon given the time. The angle is given in radians.
-    % range = 1: Angles from 0 to 2pi
-    % range = 0: Angles from -pi to pi
-    % days is just used as a substitute of "floor" function
-    % hdays is the same as days but to be used when range = 0
-    % -------------------------------------------------------------------
-    
+% This file contains a function that computes the position of the Sun
+% w.r.t. the Moon at a given time
+
+% phiSun = ComputephiSunAbsolute (t, days, hdays, rango)
+
+% Inputs:   t: time (counting since beginning of sim)
+%           days: number of periods since beginning
+%           hdays: number of half periods since beginning
+%           rango:  0 for angle between -pi and pi
+%                   1 for angles between 0 and 2*pi
+
+% Output:   phiSun: orientation of the Sun w.r.t the Moon
+
+function phiSun = ComputephiSunAbsolute (t, days, hdays, rango)    
     T = 29.5;               % Period
     wSun = 2*pi/T/24/3600;  % Angular speed
-    if (rango == 1)         % Code for angles from 0 to 2pi
+    if (rango == 1)
         phiSun = t*wSun - 2*pi*days;
     elseif (rango == 0)
         phiSun = t*wSun - 2*pi*(hdays-days);
