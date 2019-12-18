672d491368ceb640f7710e8683aa2442e24ae0eb
CeciliaMartinezMartin <43567723+CeciliaMartinezMartin@users.noreply.github.com>
2019-12-18 10:12:14 +0100

    Clean Code

--- ComputephiRover.m
+++ ComputephiRover.m
@@ -1,30 +1,12 @@
+% This file contains the function for knowing the angle of the Rover w.r.t. the Moon given its equation
+
+% function phiSun = ComputephiRover (data, t)
+
+% Inputs:   data: equation of evolution of angle
+%           t: time (since beginning of sim)
+
+% Output:   phiSun: angle of Rover w.r.t. the Moon (rad)
+
 function phiSun = ComputephiRover (data, t)
-    % The following function computes the angle throught the whole control
-    % horizon, for timesteps of size ts
-    % ------------------------------------------------------------
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % NEED TO DO THE THING THAT I HAVE AT HOME WITH THE LENGTH AND THE
-    % ANGLES
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%     xRover = f(0:ts:Tcontrol);    % x 
-    
-    % Implementation with a look-up table:
-    % iteration     Phi_180    Phi_360
-    %--------------------------------------
-    %   1             0           0
-    %   2             -10         350
-    %   ...           ...         ...
-%     if (rango == 0)
-%         phiSun = data (iter, 1);
-%     elseif (rango == 1)
-%         phiSun = data (iter, 2);
-%     else
-%         error('The range is not correct');
-%     end
-    
-    %% Implementation solving the angle for each time
             phiSun = data(t);
-    
-%     phiSun = 5*sin(f/3);
 end
